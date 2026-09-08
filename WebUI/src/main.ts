035a03d0c39a46fd97dc7b5592c4ea166b7d6b74
albasimia <39647095+albasimia@users.noreply.github.com>
2026-09-09 00:58:24 +0900

    style: load Shortcuts device map styles

--- WebUI/src/main.ts
+++ WebUI/src/main.ts
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import { router } from './router'
 import './styles.css'
+import './shortcuts.css'
 
 createApp(App)
   .use(createPinia())
