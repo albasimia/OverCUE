0a3413d5c6fdfde913f5b50e2d6d5a73a243a1ee
albasimia <39647095+albasimia@users.noreply.github.com>
2026-09-09 00:57:09 +0900

    feat: route Shortcuts to live device view

--- WebUI/src/router.ts
+++ WebUI/src/router.ts
@@ -3,11 +3,13 @@ import DevicesView from './views/DevicesView.vue'
 import GroupPresetsView from './views/GroupPresetsView.vue'
 import PresetsView from './views/PresetsView.vue'
 import SettingsView from './views/SettingsView.vue'
+import ShortcutsView from './views/ShortcutsView.vue'
 
 export const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', redirect: '/presets' },
+    { path: '/', redirect: '/shortcuts' },
+    { path: '/shortcuts', name: 'shortcuts', component: ShortcutsView },
     { path: '/presets', name: 'presets', component: PresetsView },
     { path: '/devices', name: 'devices', component: DevicesView },
     { path: '/group-presets', name: 'group-presets', component: GroupPresetsView },
